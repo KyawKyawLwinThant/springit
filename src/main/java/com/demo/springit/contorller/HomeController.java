9e42ef119225aa7aa68abfa05c8921d39c7a87ea
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2019-07-29 09:27:58 +0630

    home controller

--- src/main/java/com/demo/springit/contorller/HomeController.java
+++ src/main/java/com/demo/springit/contorller/HomeController.java
@@ -1,12 +1,13 @@
 package com.demo.springit.contorller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
   @GetMapping("/")
   public String home(){
-    return "Hello, World!";
+    return "home";
   }
 }
